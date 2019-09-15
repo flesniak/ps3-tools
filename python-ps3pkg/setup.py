e8f566196edc33b19db2afc007985981adbfa9c1
Fabian Lesniak <fabian@lesniak-it.de>
2019-09-15 12:56:31 +0200

    add python-ps3pkg (ps3py from ps3xploit-resigner)

---
+++ python-ps3pkg/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2
+from distutils.core import setup, Extension
+
+module1 = Extension('pkgcrypt', sources = ['crypt.c'])
+
+setup (name = 'pkgcrypt',
+       version = '1.0',
+       description = 'C implementation of the crypt function from pkg.py',
+       ext_modules = [module1])
